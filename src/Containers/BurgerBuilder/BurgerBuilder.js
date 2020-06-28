557a63b7caa23b6fba34a8c5f3ef888a33f37786
saurabhpathak072 <saurabhspathak072@gmail.com>
2020-06-28 23:41:24 +0530

    unmount interceptor

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -26,16 +26,26 @@ const INGREDIENT_PRICES={
     // }
 
     state ={
-        ingredients:{
-            salad:0,
-            bacon:0,
-            cheese:0,
-            meat:0
-        },
+        ingredients:null,
         totalPrice:4,
         purchasable:false,
         purchasing:false,
-        loading:false
+        loading:false,
+        error:false
+    }
+
+    componentDidMount(){
+        Axios.get('https://react-myburger-123.firebaseio.com/ingredients.json')
+        .then(response=>{
+            this.setState({
+                ingredients:response.data
+            });
+        })
+        .catch(error=>{
+            this.setState({
+                error:true
+            })
+        });
     }
 
     updatePurchaseState =(ingredients)=>{
@@ -123,12 +133,33 @@ const INGREDIENT_PRICES={
         for (let keys in disabledInfo){
             disabledInfo[keys]=disabledInfo[keys]<=0
         }
-        let orderSummary = <OrderSummary
-        ingredients={this.state.ingredients}
-        price={this.state.totalPrice}
-        purchaseCanceled={this.purchaseCancelHandler}
-        purchaseContinue={this.purchaseContinueHandler}
-        />
+
+        let orderSummary =null;
+
+       
+        let burger =this.state.error ? <p>Ingredients Can't be loaded</p>:<Spinner />
+        
+        if(this.state.ingredients){
+            burger =(
+                <>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls
+                 ingredientAdded={this.addIngredientHandler}
+                 ingredientRemoved={this.removeIngredientHandler}
+                 disabled={disabledInfo}
+                 purchasable={this.state.purchasable}
+                 price={this.state.totalPrice}
+                 ordered={this.purchaseHandler}
+                />
+                </>
+            );
+            orderSummary = <OrderSummary
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCanceled={this.purchaseCancelHandler}
+            purchaseContinue={this.purchaseContinueHandler}
+            />
+        }
         if(this.state.loading){
             orderSummary =<Spinner />;
         }
@@ -138,15 +169,7 @@ const INGREDIENT_PRICES={
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
-               <Burger ingredients={this.state.ingredients}/>
-               <BuildControls
-                ingredientAdded={this.addIngredientHandler}
-                ingredientRemoved={this.removeIngredientHandler}
-                disabled={disabledInfo}
-                purchasable={this.state.purchasable}
-                price={this.state.totalPrice}
-                ordered={this.purchaseHandler}
-               />
+                {burger}
             </>
         )
     }
--- src/HOC/withErrorHandler/withErrorHandler.js
+++ src/HOC/withErrorHandler/withErrorHandler.js
@@ -6,14 +6,18 @@ function withErrorHandler(WrappedComponent,axios) {
         state={
             error:null
         }
-        componentDidMount(){
-            axios.interceptors.request.use(req=>{
+        componentWillMount(){
+            this.reqInterceputor=axios.interceptors.request.use(req=>{
                 this.setState({error:null});
                 return req;
             });
-            axios.interceptors.response.use(res=>res,error=>{
+            this.resInterceputor=axios.interceptors.response.use(res=>res,error=>{
                 this.setState({error});
             });
+        }
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqInterceputor);
+            axios.interceptors.response.eject(this.resInterceputor);
         }
         errorConfirmHandler =()=>{
             this.setState({error:null});
